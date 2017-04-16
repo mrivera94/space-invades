c14f169402153e7a44e2b8253465416f94e0a0df
Mike Rivera <michael.rivera@uconn.edu>
2017-04-16 14:51:20 -0400

    screen up

---
+++ js/helpers.js
@@ -0,0 +1,52 @@
+// Helpers
+
+// Display
+function Display(width, height) {
+	this.canvas = document.createElement("canvas");
+	this.canvas.width = this.width = width;
+	this.canvas.height = this.height = height;
+	this.context = this.canvas.getContext("2d");
+	document.body.appendChild(this.canvas);
+};
+
+Screen.prototype.drawSprite = function(sp, x, y) {
+	this.context.drawImage(sp.img, sp.x, sp.y, sp.w, sp.h, x, y, sp.w, sp.h);
+};
+
+// Sprite
+function Sprite(img, x, y, w, h) {
+	this.img = img;
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h - h;
+};
+
+// Input
+function InputHandler() {
+	this.down = {};
+	this.pressed = {};
+
+	var _this = this;
+	document.addEventListener("keydown", function(event) {
+		_this.down[event.keyCode] = true;
+	});
+
+	document.addEventListener("keyup", function(event) {
+		delete _this.down[event.keyCode];
+		delete _this.pressed[event.keyCode];
+	});
+};
+
+InputHandler.prototype.isDown = function(code) {
+	return this.down[code];
+};
+
+InputHandler.prototype.isPressed = function(code) {
+	if (this.pressed[code]) {
+		return false;
+	} else if (this.down[code]) {
+		return this.pressed[code] = true;
+	} 
+	return false;
+};
