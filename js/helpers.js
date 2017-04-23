94287d5a77d2d973727fd10fdc151d73c484a8fe
Mike Rivera <michael.rivera@uconn.edu>
2017-04-23 19:53:07 -0400

    they die

--- js/helpers.js
+++ js/helpers.js
@@ -5,7 +5,7 @@
 
 function ABIntersection(ax, ay, aw, ah, bx, by, bw, bh) {
 	return ax < bx+bw && bx < ax+aw && ay < by+bh && by < ay+ah;
-}
+};
 
 function Bullet(x, y, velocity, w, h, color) {
 	this.x = x;
